5e00f960263a80f82c3875aaa7b52af51d40ff99
Yesoda Bhargava <yesoda99bhargava@gmail.com>
2022-08-06 16:41:18 +0530

    Add files via upload

---
+++ Module and Custom Distribution Package/myModules2/README.txt
@@ -0,0 +1,3 @@
+The package searches for :
+1. vowels in an input sentence. (search4vowels)
+2. user-given letters in an input sentence. (search4letters)
---
+++ Module and Custom Distribution Package/myModules2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='v_search_demo',   # most important argument 1
+    version='1.0',
+    description='Search tools',
+    author='Random',
+    author_email='random@gmail.com',
+    url='random.com',
+    py_modules=['v_search_demo']   # most important argument 2
+)
